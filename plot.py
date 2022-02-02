618565d0606f40d8fcda6b095666d3294a03ed56
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-02-01 23:35:08 -0500

    add plotting scripts for combined data

--- plot.py
+++ plot.py
@@ -8,7 +8,7 @@ import plotly as pt
 import plotly.express as px
 import plotly.graph_objects as go
 
-VERSION = '0.0.1'
+VERSION = '0.2.0'
 
 def make_square(func):
     def wrapper(*args, **kwargs):
@@ -40,6 +40,7 @@ def plot_dihedrals(df):
     fig.update_yaxes(range=[-185, 185])
     fig.update_xaxes(range=[-185, 185])
     return fig
+
 @make_square
 def plot_dihedral_by_chain(df, resname, chainids = [0, 1], plot_type = 'scatter'):
     x = f'{resname}_Chain {chainids[0]}'
@@ -57,19 +58,6 @@ def plot_dihedral_by_chain(df, resname, chainids = [0, 1], plot_type = 'scatter'
                                  y=y,
                                  marginal_x="histogram",
                                  marginal_y="histogram")
-
-    # fig.update_layout(
-    #     xaxis=dict(
-    #         tickmode='linear',
-    #         tick0=-180,
-    #         dtick=25,
-    #     ),
-    #     yaxis=dict(
-    #         tickmode='linear',
-    #         tick0=-180,
-    #         dtick=25
-    #     )
-    # )
     return fig
 
 @dist_hist_wrapper
@@ -98,5 +86,41 @@ def plot_dihedral_by_chain_histogram(df, resname, chainids = [0, 1]):
                              )
     return fig
 
-def plot_state_probabilities(prob_dict):
-    pass
+def plot_replicate_df(replicate_df):
+    fig = px.strip(replicate_df, x='Dist Name', y='Mean Val (Å)', color='Sys Name',
+                        category_orders={ # replaces default order by column name
+                    "Dist Name": ["ILE46_0 to ILE271_1", "ILE46_0 to ILE271_0", "ILE46_0 to ILE46_1", "ILE271_0 to ILE271_1"],
+                    "Sys Name": ["Open CHARMM-GUI", "Open CGUI 10x + 100ns bb", "Closed CHARMM-GUI", "Closed CGUI 10x + 100ns bb"]
+                })
+    fig.update_xaxes(title='Residue-Residue Distance')
+    fig.update_yaxes(title='Mean CA-CA Distance (Å)')
+    return fig
+
+def plot_combined_dihedral_plots(df, resname, chainids = [0, 1]):
+    x = f'{resname}_Chain {chainids[0]}'
+    y = f'{resname}_Chain {chainids[1]}'
+    fig = px.density_heatmap(df,
+                             x=x,
+                             y=y,
+                             nbinsx=72,
+                             nbinsy=72,
+    #                          marginal_x="histogram",
+    #                          marginal_y="histogram",
+                                   color_continuous_scale='YlGnBu',
+                             range_x=[-180, 180],
+                             range_y=[-180, 180],
+                             facet_col='Sys Name',
+                                  category_orders={ # replaces default order by column name
+    #                     "Dist Name": ["ILE46_0 to ILE271_1", "ILE46_0 to ILE271_0", "ILE46_0 to ILE46_1", "ILE271_0 to ILE271_1"],
+                        "Sys Name": ["Open CHARMM-GUI", "Open CGUI 10x + 100ns bb", "Closed CHARMM-GUI", "Closed CGUI 10x + 100ns bb"]
+                    }
+                             )
+    fig.update_layout(height=400, width=1200)
+    fig.update_xaxes(range=[-135, 185], nticks=20)
+    fig.update_yaxes(range=[-135, 185], nticks=20)
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    # fig.update_yaxes(
+    #     scaleanchor="x",
+    #     scaleratio=1,
+    # )
+    return fig
